cd7abf1c9b5cd2a5dedfa77eae33a1159fcf5796
d1nz-g33k <d1nz-g33k@users.noreply.github.com>
2019-04-19 14:43:25 +0530

    More Functions
    Functions added, Indentation cahnged to tabs.
    
    HTML, CSS added along with some javascript changes

--- DFS.py
+++ DFS.py
@@ -5,7 +5,7 @@ from flask import Flask, request, session, g, redirect, url_for, abort, render_t
 from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 from werkzeug.utils import secure_filename
 import socket
-
+import thread
 
 if __name__ == "__main__":
 	app.run()
@@ -50,78 +50,78 @@ def close_db(db):
 @app.route('/login', methods=['GET', 'POST'])
 def home():
 	db=connect_db()
-    c=db.cursor()
-    c.execute('SELECT Count() FROM ')
-    R = c.fetchone()[0]
+	c=db.cursor()
+	c.execute('SELECT Count() FROM ')
+	R = c.fetchone()[0]
 
-    if request.method == 'POST':
-        g_usr=request.form.get('ID', None)
-        if request.form.get('ID', None) != (app.config['USERNAME']):
-            c.execute('SELECT Count() FROM Users')
-            N=c.fetchone()
-            c.execute('SELECT TID FROM Users')
-            r=c.fetchall()
-            for i in range(0,N[0]):
-                m=str(r[i])
-                q=request.form.get('ID', None)
-                p="('"+q+"',)"
-                if p == m:
-                    M=[m]
-                    c.execute('SELECT pswd FROM Users WHERE uname=?', (q,))
-                    pswd=c.fetchone()[0]
-                    if(request.form.get('pswd', None) == pswd):
-                        session['User'] = True
-                        connect_server()
-                        return redirect(url_for('user'))
-                    else:
-                        E='Invalid password'
+	if request.method == 'POST':
+		g_usr=request.form.get('ID', None)
+		if request.form.get('ID', None) != (app.config['USERNAME']):
+			c.execute('SELECT Count() FROM Users')
+			N=c.fetchone()
+			c.execute('SELECT TID FROM Users')
+			r=c.fetchall()
+			for i in range(0,N[0]):
+				m=str(r[i])
+				q=request.form.get('ID', None)
+				p="('"+q+"',)"
+				if p == m:
+					M=[m]
+					c.execute('SELECT pswd FROM Users WHERE uname=?', (q,))
+					pswd=c.fetchone()[0]
+					if(request.form.get('pswd', None) == pswd):
+						session['User'] = True
+						connect_server()
+						return redirect(url_for('user'))
+					else:
+						E='Invalid password'
 
-        elif request.form.get('pswd', None) != app.config['PASSWORD']:
-            F='Invalid password'
-            
-        else:
-            session['Admin'] = True
-            return redirect(url_for('admin'))
-            
-    close_db(db)
-    return render_template('Login.html')
+		elif request.form.get('pswd', None) != app.config['PASSWORD']:
+			F='Invalid password'
+			
+		else:
+			session['Admin'] = True
+			return redirect(url_for('admin'))
+			
+	close_db(db)
+	return render_template('Login.html')
 
 @app.route('/logout', methods=['GET', 'POST'])
 def logout():
-    session['User'] = False
-    session['Admin'] = False
-    return home()
+	session['User'] = False
+	session['Admin'] = False
+	return home()
 
 
 @app.route('/admin', methods=['GET', 'POST'])
 def admin():
-    if(session['Admin'] != True):
-        return render_template('login.html')
-    else:
-        return render_template('admin_dashboard.html')
+	if(session['Admin'] != True):
+		return render_template('login.html')
+	else:
+		return render_template('admin_dashboard.html')
 
 
 @app.route('/add_user', methods=['GET', 'POST'])
 def add_user():
-    db=connect_db()
-    c=db.cursor()
+	db=connect_db()
+	c=db.cursor()
 
-    r=1
-    
-    if request.method == 'POST':
-        fn=request.form.get('firstname')
-        ln=request.form.get('lastname')
-        g=request.form.get('gender')
-        un = request.form.get('uname')
+	r=1
+	
+	if request.method == 'POST':
+		fn=request.form.get('firstname')
+		ln=request.form.get('lastname')
+		g=request.form.get('gender')
+		un = request.form.get('uname')
 
-        c.execute("INSERT into Users VALUES( ?, ?, ?, ?)", (ln, fn, g, un,))
+		c.execute("INSERT into Users VALUES( ?, ?, ?, ?)", (ln, fn, g, un,))
 
 
-    close_db(db)
-    if(session['Admin'] != True):
-        return render_template('login.html')
-    else:
-        return render_template('New_User.html', adno=r)
+	close_db(db)
+	if(session['Admin'] != True):
+		return render_template('login.html')
+	else:
+		return render_template('New_User.html', adno=r)
 
 def upload_file():
 	# To upload files to the server
@@ -131,6 +131,28 @@ def make_server():
 	# To make a server *Accessible by admin only
 
 
+def initiate_server():
+	# To start the server
+	s = socket.socket()         # Create a socket object
+	host = socket.gethostname() # Get local machine name
+	port = 50000                # Reserve a port for your service.
+
+	print 'Server started!'
+	print 'Waiting for clients...'
+
+	s.bind((host, port))        # Bind to the port
+	s.listen(5)                 # Now wait for client connection.
+
+	print 'Got connection from', addr
+	while True:
+	   c, addr = s.accept()     # Establish connection with client.
+	   thread.start_new_thread(on_new_client,(c,addr))
+	   #Note it's (addr,) not (addr) because second parameter is a tuple
+	   #Edit: (c,addr)
+	   #that's how you pass arguments to functions when creating new threads using thread module.
+	s.close()
+
+
 def close_server():
 	# End a server
 
@@ -138,3 +160,18 @@ def close_server():
 def connect_server():
 	# To connect to the server *For User
 
+
+def on_new_client(clientsocket,addr):
+	# Actions done everytime a new user connects
+	while True:
+		msg = clientsocket.recv(1024)
+		#do some checks and if msg == someWeirdSignal: break:
+		print addr, ' >> ', msg
+		msg = raw_input('SERVER >> ')
+		#Maybe some code to compute the last digit of PI, play game or anything else can go here and when you are done.
+		clientsocket.send(msg)
+	clientsocket.close()
+
+
+def remove_server():
+	# To remove the system from the server list
