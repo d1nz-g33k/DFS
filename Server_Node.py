0f8128e13f57a36bffc07b411adc7f11c86ad5e5
d1nz-g33k <d1nz-g33k@users.noreply.github.com>
2019-04-29 23:10:11 +0530

    Server_Node almost done
    Added almost everything except upload_file, remove & restore functions.
    -> Also to add a function to create a shared resource (memory)

--- Server_Node.py
+++ Server_Node.py
@@ -5,7 +5,7 @@ from flask import Flask, request, session, g, redirect, url_for, abort, render_t
 from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 from werkzeug.utils import secure_filename
 import socket
-import thread
+# import thread
 
 if __name__ == "__main__":
 	app.run()
@@ -28,20 +28,20 @@ app.config.update(dict(
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 
+
 def connect_db():
 	"""Connects to the Database."""
 	db = sql.connect(app.config['DATABASE'])
-	db.execute('CREATE TABLE IF NOT EXISTS Users()')	
+	db.execute('CREATE TABLE IF NOT EXISTS Users(UID VARCHAR PRIMARY KEY NOT NULL, LOC VARCHAR NOT NULL, fn VARCHAR NOT NULL, ln VARCHAR NOT NULL, g VARCHAR NOT NULL)')	
 	# We need a table for maintaining the Users list
 	
 	# db.execute('CREATE TABLE IF NOT EXISTS Files()')
 	# A table to track the changes made to the Files **Time, Date, Users, file-type
 
-	db.execute('CREATE TABLE IF NOT EXISTS Servers()')
+	# db.execute('CREATE TABLE IF NOT EXISTS Servers()')
 	# Maintain details of data servers
 	
 	return db
-
 
 def close_db(db):
 	db.commit()
@@ -50,10 +50,9 @@ def close_db(db):
 
 @app.route('/login/<uid>', methods=['GET', 'POST'])
 def home(uid):
+
 	db=connect_db()
 	c=db.cursor()
-	c.execute('SELECT Count() FROM ')
-	R = c.fetchone()[0]
 
 	if request.method == 'POST':
 		c.execute('SELECT Count() FROM Users')
@@ -69,13 +68,20 @@ def home(uid):
 				c.execute('SELECT pswd FROM Users WHERE uid=?', (q,))
 				pswd=c.fetchone()[0]
 				if(request.form.get('pswd', None) == pswd):
-					session['User'] = True
+					session[uid] = True
 					connect_server()
 					return redirect(url_for('user'))
 				else:
 					E='Invalid password'
 	close_db(db)
-	return render_template('Login.html')
+	return render_template('Node_Login.html')
+
+@app.route('/login')
+def re_route():
+	# Change later
+	# Route to load_balancer
+	return redirect('https://www.google.com')
+
 
 @app.route('/logout', methods=['GET', 'POST'])
 def logout():
@@ -92,81 +98,61 @@ def admin():
 		return render_template('node_admin_dashboard.html')
 
 
-@app.route('/add_user', methods=['GET', 'POST'])
-def add_user():
-	db=connect_db()
-	c=db.cursor()
-
-	r=1
-	
-	if request.method == 'POST':
-		fn=request.form.get('firstname')
-		ln=request.form.get('lastname')
-		g=request.form.get('gender')
-		un = request.form.get('uname')
-
-		c.execute("INSERT into Users VALUES( ?, ?, ?, ?)", (ln, fn, g, un,))
-
-
-	close_db(db)
-	if(session['Admin'] != True):
-		return render_template('login.html')
-	else:
-		return render_template('New_User.html', adno=r)
+@app.route('/user/<uid>', methods=['GET', 'POST'])
+def user(uid):
+	# if(session[uid] != True):
+	# 	return redirect(url_for('home'))
+	# create a session for the respective uid at login
+	return render_template('user.html', uid=uid)
 
 def upload_file():
 	# To upload files to the server
+	pass
 
+def delete_file():
+	# To delete files from the server
+	pass
+
+def restore_file():
+	# To restore files to the server
+	pass
 
 def data_node():
 	# To make a server *Accessible by admin only
+	pass
 
 
-def initiate_server():
-	# To start the server
-	# Everytime a server starts the number of servers running is to be updated
-	# Also the database must be updated to the latest version.  
-	s = socket.socket()         # Create a socket object
-	host = socket.gethostname() # Get local machine name
-	port = 50000                # Reserve a port for your service.
+# def initiate_server():
+# 	# To start the server
+# 	# Everytime a server starts the number of servers running is to be updated
+# 	# Also the database must be updated to the latest version.  
+# 	s = socket.socket()         # Create a socket object
+# 	host = socket.gethostname() # Get local machine name
+# 	port = 50000                
+# 	# Reserve a port for your service.
 
-	print 'Server started!'
-	print 'Waiting for clients...'
+# 	print ('Server started!')
+# 	print ('Waiting for clients...')
 
-	s.bind((host, port))        # Bind to the port
-	s.listen(5)                 # Now wait for client connection.
+# 	s.bind((host, port))        # Bind to the port
+# 	s.listen(5)                 # Now wait for client connection.
 
-	print 'Got connection from', addr
-	while True:
-	   c, addr = s.accept()     # Establish connection with client.
-	   thread.start_new_thread(on_new_client,(c,addr))
-	   #Note it's (addr,) not (addr) because second parameter is a tuple
-	   #Edit: (c,addr)
-	   #that's how you pass arguments to functions when creating new threads using thread module.
-	s.close()
+# 	print ('Got connection from', addr)
+# 	while True:
+# 	   c, addr = s.accept()     # Establish connection with client.
+# 	   thread.start_new_thread(on_new_client,(c,addr))
+# 	   #Note it's (addr,) not (addr) because second parameter is a tuple
+# 	   #Edit: (c,addr)
+# 	   #that's how you pass arguments to functions when creating new threads using thread module.
+# 	s.close()
 
-
-def end_data_node():
-	# End a server
-
-
-def connect_server():
-	# To connect to the server *For User
-
-
-def on_new_client(clientsocket,addr):
-	# Actions done everytime a new user connects
-	while True:
-		msg = clientsocket.recv(1024)
-		#do some checks and if msg == someWeirdSignal: break:
-		print addr, ' >> ', msg
-		msg = raw_input('SERVER >> ')
-		#Maybe some code to compute the last digit of PI, play game or anything else can go here and when you are done.
-		clientsocket.send(msg)
-	clientsocket.close()
-
-
-def remove_server():
-	# To remove the system from the server list
-
-
+# def on_new_client(clientsocket,addr):
+# 	# Actions done everytime a new user connects
+# 	while True:
+# 		msg = clientsocket.recv(1024)
+# 		#do some checks and if msg == someWeirdSignal: break:
+# 		print (addr, ' >> ', msg)
+# 		msg = raw_input('SERVER >> ')
+# 		#Maybe some code to compute the last digit of PI, play game or anything else can go here and when you are done.
+# 		clientsocket.send(msg)
+# 	clientsocket.close()
